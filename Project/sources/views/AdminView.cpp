f05bff632a03757e5d220b15bd5694c536399a94
goncaloribeiro1234 <1241164@isep.ipp.pt>
2026-06-09 17:21:24 +0100

    View das classes

--- Project/sources/views/AdminView.cpp
+++ Project/sources/views/AdminView.cpp
@@ -5,11 +5,9 @@
 using namespace std;
 
 void AdminView::showMenu() {
-
     int opt = -1;
 
     while (opt != 0) {
-
         cout << "\n--- MENU ADMINISTRADOR ---" << endl;
         cout << "1. Registar Atleta" << endl;
         cout << "2. Registar Instrutor" << endl;
@@ -19,13 +17,14 @@ void AdminView::showMenu() {
         cout << "6. Listar Todos os Instrutores" << endl;
         cout << "7. Listar Todas as Salas" << endl;
         cout << "8. Listar Modalidades" << endl;
+        cout << "9. Criar Aula" << endl;
+        cout << "10. Listar Aulas" << endl;
         cout << "0. Voltar" << endl;
 
         cout << "Opcao: ";
         cin >> opt;
 
         if (opt == 1) {
-
             string name;
             string email;
             string pass;
@@ -49,11 +48,11 @@ void AdminView::showMenu() {
             int levelOption;
             cin >> levelOption;
 
-            if(levelOption == 1)
+            if (levelOption == 1)
                 level = "Iniciante";
-            else if(levelOption == 2)
+            else if (levelOption == 2)
                 level = "Intermedio";
-            else if(levelOption == 3)
+            else if (levelOption == 3)
                 level = "Avancado";
             else {
                 cout << "Nivel invalido." << endl;
@@ -61,26 +60,20 @@ void AdminView::showMenu() {
             }
 
             try {
-
                 athleteController.addAthlete(
-                        name,
-                        email,
-                        pass,
-                        level
+                    name,
+                    email,
+                    pass,
+                    level
                 );
 
                 cout << "SUCESSO: Atleta registado!" << endl;
-            }
-            catch(exception& e) {
-
+            } catch (exception &e) {
                 cout << "ERRO: "
-                     << e.what()
-                     << endl;
+                        << e.what()
+                        << endl;
             }
-        }
-
-        else if (opt == 2) {
-
+        } else if (opt == 2) {
             string name;
             string email;
             string pass;
@@ -106,8 +99,7 @@ void AdminView::showMenu() {
 
             vector<string> modalityNames;
 
-            for (auto m : modalidades) {
-
+            for (auto m: modalidades) {
                 cout
                         << index
                         << ". "
@@ -115,7 +107,7 @@ void AdminView::showMenu() {
                         << endl;
 
                 modalityNames.push_back(
-                        m->getName()
+                    m->getName()
                 );
 
                 index++;
@@ -126,27 +118,23 @@ void AdminView::showMenu() {
             int modChoice;
             cin >> modChoice;
 
-            if(modChoice > 0 &&
-               modChoice <= modalityNames.size()) {
-
+            if (modChoice > 0 &&
+                modChoice <= modalityNames.size()) {
                 specialty =
                         modalityNames[
-                                modChoice - 1
+                            modChoice - 1
                         ];
-            }
-            else {
-
+            } else {
                 cout << "Opcao invalida." << endl;
                 specialty = "Indefinida";
             }
 
             try {
-
                 instructorController.addInstructor(
-                        name,
-                        email,
-                        pass,
-                        specialty
+                    name,
+                    email,
+                    pass,
+                    specialty
                 );
 
                 cout
@@ -154,18 +142,13 @@ void AdminView::showMenu() {
                         << specialty
                         << " registado!"
                         << endl;
-            }
-            catch(exception& e) {
-
+            } catch (exception &e) {
                 cout
                         << "ERRO: "
                         << e.what()
                         << endl;
             }
-        }
-
-        else if (opt == 3) {
-
+        } else if (opt == 3) {
             string name;
             int capacity;
 
@@ -178,25 +161,19 @@ void AdminView::showMenu() {
             cin >> capacity;
 
             try {
-
                 roomController.addRoom(
-                        name,
-                        capacity
+                    name,
+                    capacity
                 );
 
                 cout << "SUCESSO: Sala criada!" << endl;
-            }
-            catch(exception& e) {
-
+            } catch (exception &e) {
                 cout
                         << "ERRO: "
                         << e.what()
                         << endl;
             }
-        }
-
-        else if (opt == 4) {
-
+        } else if (opt == 4) {
             string name;
             string description;
             string minimumLevel;
@@ -218,11 +195,11 @@ void AdminView::showMenu() {
             int levelOption;
             cin >> levelOption;
 
-            if(levelOption == 1)
+            if (levelOption == 1)
                 minimumLevel = "Iniciante";
-            else if(levelOption == 2)
+            else if (levelOption == 2)
                 minimumLevel = "Intermedio";
-            else if(levelOption == 3)
+            else if (levelOption == 3)
                 minimumLevel = "Avancado";
             else {
                 cout << "Nivel invalido." << endl;
@@ -230,18 +207,15 @@ void AdminView::showMenu() {
             }
 
             modalityController.createModality(
-                    name,
-                    description,
-                    minimumLevel
+                name,
+                description,
+                minimumLevel
             );
 
             cout
                     << "SUCESSO: Modalidade criada!"
                     << endl;
-        }
-
-        else if (opt == 5) {
-
+        } else if (opt == 5) {
             auto lista =
                     athleteController.findAllAthletes();
 
@@ -249,8 +223,7 @@ void AdminView::showMenu() {
                     << "\n--- LISTAGEM DE ATLETAS ---"
                     << endl;
 
-            for(auto a : lista) {
-
+            for (auto a: lista) {
                 cout
                         << "Nome: "
                         << a->getName()
@@ -260,10 +233,7 @@ void AdminView::showMenu() {
 
                         << endl;
             }
-        }
-
-        else if (opt == 6) {
-
+        } else if (opt == 6) {
             auto instrutores =
                     instructorController.findAllInstructors();
 
@@ -271,15 +241,13 @@ void AdminView::showMenu() {
                     << "\n--- LISTAGEM DE INSTRUTORES ---"
                     << endl;
 
-            if(instrutores.empty()) {
-
+            if (instrutores.empty()) {
                 cout
                         << "Ainda nao ha instrutores registados."
                         << endl;
             }
 
-            for(auto i : instrutores) {
-
+            for (auto i: instrutores) {
                 cout
                         << "Nome: "
                         << i->getName()
@@ -289,10 +257,7 @@ void AdminView::showMenu() {
 
                         << endl;
             }
-        }
-
-        else if (opt == 7) {
-
+        } else if (opt == 7) {
             auto salas =
                     roomController.findAllRooms();
 
@@ -300,15 +265,13 @@ void AdminView::showMenu() {
                     << "\n--- LISTAGEM DE SALAS ---"
                     << endl;
 
-            if(salas.empty()) {
-
+            if (salas.empty()) {
                 cout
                         << "Ainda nao ha salas registadas."
                         << endl;
             }
 
-            for(auto r : salas) {
-
+            for (auto r: salas) {
                 cout
                         << "Sala: "
                         << r->getName()
@@ -319,10 +282,7 @@ void AdminView::showMenu() {
                         << " pessoas"
                         << endl;
             }
-        }
-
-        else if (opt == 8) {
-
+        } else if (opt == 8) {
             auto modalidades =
                     modalityController.findAllModalities();
 
@@ -330,15 +290,13 @@ void AdminView::showMenu() {
                     << "\n--- LISTAGEM DE MODALIDADES ---"
                     << endl;
 
-            if(modalidades.empty()) {
-
+            if (modalidades.empty()) {
                 cout
                         << "Ainda nao ha modalidades registadas."
                         << endl;
             }
 
-            for(auto m : modalidades) {
-
+            for (auto m: modalidades) {
                 cout
                         << "Nome: "
                         << m->getName()
@@ -348,6 +306,200 @@ void AdminView::showMenu() {
 
                         << " | Nivel Minimo: "
                         << m->getMinimumLevel()
+
+                        << endl;
+            }
+        } else if (opt == 9) {
+            auto modalidades =
+                    modalityController.findAllModalities();
+
+            auto instrutores =
+                    instructorController.findAllInstructors();
+
+            auto salas =
+                    roomController.findAllRooms();
+
+            string modality;
+            string instructor;
+            string room;
+
+            string date;
+            string startTime;
+            string endTime;
+
+            int duration;
+
+            cout << "\n--- CRIAR AULA ---" << endl;
+
+            // MODALIDADES
+
+            cout << "\nSelecione a Modalidade:" << endl;
+
+            vector<string> modalityNames;
+
+            int index = 1;
+
+            for (auto m: modalidades) {
+                cout
+                        << index
+                        << ". "
+                        << m->getName()
+                        << endl;
+
+                modalityNames.push_back(
+                    m->getName()
+                );
+
+                index++;
+            }
+
+            int modChoice;
+
+            cout << "Opcao: ";
+            cin >> modChoice;
+
+            modality =
+                    modalityNames[modChoice - 1];
+
+            // INSTRUTORES
+
+            cout << "\nSelecione o Instrutor:" << endl;
+
+            vector<string> instructorNames;
+
+            index = 1;
+
+            for (auto i: instrutores) {
+                cout
+                        << index
+                        << ". "
+                        << i->getName()
+
+                        << " ("
+                        << i->getSpecialty()
+                        << ")"
+
+                        << endl;
+
+                instructorNames.push_back(
+                    i->getName()
+                );
+
+                index++;
+            }
+
+            int instructorChoice;
+
+            cout << "Opcao: ";
+            cin >> instructorChoice;
+
+            instructor =
+                    instructorNames[
+                        instructorChoice - 1
+                    ];
+
+            // SALAS
+
+            cout << "\nSelecione a Sala:" << endl;
+
+            vector<string> roomNames;
+
+            index = 1;
+
+            for (auto r: salas) {
+                cout
+                        << index
+                        << ". "
+                        << r->getName()
+
+                        << " ("
+                        << r->getCapacity()
+                        << " pessoas)"
+
+                        << endl;
+
+                roomNames.push_back(
+                    r->getName()
+                );
+
+                index++;
+            }
+
+            int roomChoice;
+
+            cout << "Opcao: ";
+            cin >> roomChoice;
+
+            room =
+                    roomNames[roomChoice - 1];
+
+            // HORARIO
+
+            cout << "\nData (DD/MM/AAAA): ";
+            cin >> date;
+
+            cout << "Hora Inicio (HH:MM): ";
+            cin >> startTime;
+
+            cout << "Hora Fim (HH:MM): ";
+            cin >> endTime;
+
+            cout << "Duracao (minutos): ";
+            cin >> duration;
+
+            classSessionController.createClassSession(
+                modality,
+                instructor,
+                room,
+                date,
+                startTime,
+                endTime,
+                duration
+            );
+
+            cout << "\nSUCESSO: Aula criada!" << endl;
+        }
+        else if (opt == 10) {
+
+            auto sessions =
+                    classSessionController.findAllSessions();
+
+            cout
+                    << "\n--- LISTAGEM DE AULAS ---"
+                    << endl;
+
+            if(sessions.empty()) {
+
+                cout
+                        << "Ainda nao ha aulas criadas."
+                        << endl;
+            }
+
+            for(auto s : sessions) {
+
+                cout
+                        << "\nModalidade: "
+                        << s->getModality()
+
+                        << "\nInstrutor: "
+                        << s->getInstructor()
+
+                        << "\nSala: "
+                        << s->getRoom()
+
+                        << "\nData: "
+                        << s->getDate()
+
+                        << "\nHora Inicio: "
+                        << s->getStartTime()
+
+                        << "\nHora Fim: "
+                        << s->getEndTime()
+
+                        << "\nDuracao: "
+                        << s->getDuration()
+
+                        << " minutos"
 
                         << endl;
             }
